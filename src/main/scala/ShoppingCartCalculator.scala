f2099b361a8e0f0a8fd6c42a2769465249ec408f
Sean Rasmussen <sean@erasmos.com>
2022-01-04 12:07:47 +0000

    [pre-interview] Step 2 / Implementation

--- src/main/scala/ShoppingCartCalculator.scala
+++ src/main/scala/ShoppingCartCalculator.scala
@@ -6,10 +6,39 @@ object ShoppingCartCalculator {
     calculate(asItemTypes(items))
 
   private def asItemTypes(items: String): Seq[ItemType]
-    = items.split(',').filterNot(_.isBlank).map(ItemTypes.resolve).toSeq
+  = items.split(',').filterNot(_.isBlank).map(ItemTypes.resolve).toSeq
 
-  private def calculate(items: Seq[ItemType]): Int =
-    items.map(_.getCostInPence).sum
+  private def calculate(items: Seq[ItemType]): Int = {
+    val (costOfApplesWithOffer, remainingNumberOfApples) = applyAppleOffer(items)
+    val (costOfOrangesWithOffer, remainingNumberOfOranges) = applyOrangeOffer(items)
+
+    val costOfApples = costOfApplesWithOffer + (remainingNumberOfApples * Apple.getCostInPence)
+    val costOfOranges = costOfOrangesWithOffer + (remainingNumberOfOranges * Orange.getCostInPence)
+
+    costOfApples + costOfOranges
+  }
+
+  /**
+   * Offer: Buy one apple, get one free
+   */
+  private def applyAppleOffer(items: Seq[ItemType]): (Int, Int) = {
+    val originalNumber = items.count(_ == Apple)
+    val costPerAppliedOfferInPence = 60
+    val costOfAllAppliedOffers = (originalNumber / 2) * costPerAppliedOfferInPence
+    val remainingNumber = originalNumber % 2
+    (costOfAllAppliedOffers, remainingNumber)
+  }
+
+  /**
+   * Buy three oranges, and get one free
+   */
+  private def applyOrangeOffer(items: Seq[ItemType]): (Int, Int) = {
+    val originalNumber = items.count(_ == Orange)
+    val costPerAppliedOfferInPence = 50
+    val costOfAllAppliedOffers = (originalNumber / 3) * costPerAppliedOfferInPence
+    val remainingNumber = originalNumber % 3
+    (costOfAllAppliedOffers, remainingNumber)
+  }
 }
 
 object ItemTypes {
@@ -17,9 +46,11 @@ object ItemTypes {
   sealed trait ItemType {
     def getCostInPence: Int
   }
+
   case object Apple extends ItemType {
     override def getCostInPence: Int = 60
   }
+
   case object Orange extends ItemType {
     override def getCostInPence: Int = 25
   }
--- src/test/scala/ShoppingCartCalculatorTest.scala
+++ src/test/scala/ShoppingCartCalculatorTest.scala
@@ -26,14 +26,14 @@ class ShoppingCartCalculatorTest extends AnyWordSpec with Matchers with BeforeAn
         "items for which there are" when {
           "no applicable offers" when {
             "with different casing" in {
-              val totalValueInPence = ShoppingCartCalculator.calculate("A,a,O,A")
+              val totalValueInPence = ShoppingCartCalculator.calculate("a,o")
 
-              totalValueInPence shouldBe 205
+              totalValueInPence shouldBe 85
             }
             "with padding" in {
-              val totalValueInPence = ShoppingCartCalculator.calculate("A,O,  O  , O  ")
+              val totalValueInPence = ShoppingCartCalculator.calculate("A,O,  O")
 
-              totalValueInPence shouldBe 135
+              totalValueInPence shouldBe 110
             }
             "with upper-casing, without padding" in {
               val totalValueInPence = ShoppingCartCalculator.calculate("A,O")
@@ -41,21 +41,34 @@ class ShoppingCartCalculatorTest extends AnyWordSpec with Matchers with BeforeAn
               totalValueInPence shouldBe 85
             }
           }
-          "a single applicable offer applied once, namely" when {
+          "a single offer applied once, namely" when {
             "buy one apple, and get one free" in {
-              pending
+              val totalValueInPence = ShoppingCartCalculator.calculate("A,A")
+
+              totalValueInPence shouldBe 60
             }
             "buy three oranges, and get one free" in {
-              pending
+              val totalValueInPence = ShoppingCartCalculator.calculate("O,O,O")
+
+              totalValueInPence shouldBe 50
             }
           }
-          "a single applicable offer applied multiple times, namely" when {
+          "a single offer applied multiple times, namely" when {
             "buy one apple, and get one free" in {
-              pending
+              val totalValueInPence = ShoppingCartCalculator.calculate("A,A,A,A,A")
+
+              totalValueInPence shouldBe 180
             }
             "buy three oranges, and get one free" in {
-              pending
+              val totalValueInPence = ShoppingCartCalculator.calculate("O,O,O,O,O,O,O")
+
+              totalValueInPence shouldBe 125
             }
+          }
+          "multiple applicable offers applied multiple times" in {
+            val totalValueInPence = ShoppingCartCalculator.calculate("A,O,O,A,A,A,O,O,O,O")
+
+            totalValueInPence shouldBe 220
           }
 
         }
